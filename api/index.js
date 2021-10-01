e85c83d7b4920640b7ff60dfc3068a7b58fd52ed
gaurav <gurumeena41.gm@gmail.com>
2021-10-01 09:03:12 +0530

    added stripe & order routes and finished back-end api

--- api/index.js
+++ api/index.js
@@ -6,6 +6,9 @@ const cors = require("cors");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const checkoutRoute = require("./routes/stripe");
 
 dotenv.config();
 
@@ -16,11 +19,14 @@ mongoose
   .then(() => console.log("Database connection successfull"))
   .catch((err) => console.log(err));
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/cart", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", checkoutRoute);
 
 app.listen(PORT || 7000, () => {
   console.log(`server is running ${PORT || 7000}`);
